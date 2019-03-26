6d6365dc1acf678a5589f9ca53bf1029504a9f03
peteraflores <Japanboysushi@gmail.com>
2019-03-25 23:53:17 -0400

    Testing requests

--- api/__init__.py
+++ api/__init__.py
@@ -3,12 +3,14 @@ from flask import Flask, request
 app = Flask(__name__)
 print("Flask running")
 # PUT CODE HERE
-
+responseDict = {}
 
 @app.route("/", methods=["POST","GET"])
 def root_post():
     if request.method == "POST":
         print(request.json)
+        responseDict = request.json
+        print(responseDict["queryResult"]["fulfillmentText"])
         return "",200
     else:
         return"""<form method="POST">
